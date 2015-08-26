c9015f21094a7d9ef4cafe207b8304778b3dd107
Isaac Laughlin <isaac.laughlin@galvanize.com>
2015-08-26 15:57:02 -0700

    Modifying rec runner to use performotron.

--- code/rec_runner.py
+++ code/rec_runner.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import graphlab as gl
-
+from performotron import Comparer
 
 def manip_sample_sub_format(sample_sub):
     """
@@ -11,12 +11,32 @@ def manip_sample_sub_format(sample_sub):
     for_pred['movie'] = sample_sub.id.apply(lambda x: int(x.split('_')[1]))
     return for_pred
 
+class RecComparer(Comparer):
+    def score(self, predictions):
+        """Look at 5% of most highly predicted movies for each movie.
+        Return the average actual rating of those movies.
+        """
+        #sample = pd.read_csv('data/sample_submission.csv')
+        
+        df = pd.concat([#sample,
+                        predictions,
+                        self.target])
+        g = df.groupby('user')
+        
+        top_5 = g.rating.transform(
+            lambda x: x>=x.quantile(.95)
+        )
+
+        return self.target[top_5].mean()
 
 if __name__ == "__main__":
     sample_sub_fname = "data/sample_submission.csv"
     ratings_data_fname = "data/training_ratings_for_kaggle_comp.csv"
     output_fname = "test_submission.csv"
 
+    test=pd.read_csv('data/dont_use.csv')
+    rc = RecComparer(test.rating, config_file='code/config.yaml')
+    
     ratings = gl.SFrame(ratings_data_fname)
     sample_sub = pd.read_csv(sample_sub_fname)
     for_prediction = manip_sample_sub_format(sample_sub)
@@ -28,4 +48,9 @@ if __name__ == "__main__":
                                                      solver='auto')
 
     sample_sub.rating = rec_engine.predict(for_prediction)
-    sample_sub.to_csv(output_fname, index=False)
+
+    rc.report_to_slack(sample_sub)
+    
+    #sample_sub.to_csv(output_fname, index=False)
+
+
