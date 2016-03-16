7c0809e411a0c91786d07431e74251c658e2993c
Isaac Laughlin <isaac.laughlin@galvanize.com>
2016-03-16 10:11:05 -0700

    Changing the data source to jokes.

--- code/rec_runner.py
+++ code/rec_runner.py
@@ -3,10 +3,10 @@ import graphlab as gl
 
 def manip_sample_sub_format(sample_sub):
     """
-    Return SFrame based on sample sub, with movid id pulled into separate format for consistency
+    Return SFrame based on sample sub, with item id pulled into separate format for consistency
     with the data used for model fitting.
     """
-    sample_sub['movie'] = sample_sub.id.apply(lambda x: int(x.split('_')[1]))
+    sample_sub['joke_id'] = sample_sub.id.apply(lambda x: int(x.split('_')[1]))
     for_prediction = gl.SFrame(sample_sub)
     return for_prediction
 
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     sample_sub = pd.read_csv(sample_sub_fname)
     for_prediction = manip_sample_sub_format(sample_sub)
     rec_engine = gl.factorization_recommender.create(   observation_data=ratings, 
-                                                        user_id="user", 
-                                                        item_id="movie", 
+                                                        user_id="user_id", 
+                                                        item_id="joke_id", 
                                                         target='rating',
                                                         num_factors=4,
                                                         solver='auto')
     
     sample_sub.rating = rec_engine.predict(for_prediction)
-    sample_sub.drop('movie', inplace=True, axis=1)
+    sample_sub.drop('joke_id', inplace=True, axis=1)
     sample_sub.to_csv(output_fname, index=False)
